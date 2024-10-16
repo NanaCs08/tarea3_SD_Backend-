981ae76aa69f962189b9e87fe8bf765097d32507
NanaCs08 <82248322+NanaCs08@users.noreply.github.com>
2024-10-15 18:38:21 -0600

    cambios para conectarme correctamente a bd

--- models/Author.js
+++ models/Author.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+    book_id: Number,
+    title: String
+}, { _id: false });
+
 const AuthorSchema = new mongoose.Schema({
-    name: String,
-    birthdate: Date,
+    id: Number,
+    author: String,
     nationality: String,
-    books: [String]
+    birth_year: Number,
+    fields: String,
+    books: [BookSchema]
 });
+
 module.exports = mongoose.model('Author', AuthorSchema);
+
--- models/Publisher.js
+++ models/Publisher.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
+
+// Define el esquema para los libros asociados a un editor
+const BookSchema = new mongoose.Schema({
+    book_id: Number,
+    title: String
+}, { _id: false });
+
 const PublisherSchema = new mongoose.Schema({
-    name: String,
+    id: Number,
+    publisher: String,
     country: String,
     founded: Number,
     genre: String,
-    booksPublished: [String]
+    books: [BookSchema]
 });
+
 module.exports = mongoose.model('Publisher', PublisherSchema);
+
